1cd4c16accd1f41836905fad27e8b610db61a973
yuguchix <115532108+yuguchix@users.noreply.github.com>
2023-03-23 16:57:11 +0900

    Add files via upload

--- pages/page3.py
+++ pages/page3.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+
 
 df = pd.read_csv('data/平均気温.csv', index_col='月')
 # st.dataframe(df)
@@ -10,8 +12,8 @@ st.line_chart(df)
 st.subheader('2021年気温')
 st.bar_chart(df['2021年'])
 
-# matplotlib
-fig, ax = plt.subplots()
-ax.plot(df.index, df['2021年'])
-ax.set_title('matplotlib graph')
-st.pyplot(fig)
+# # matplotlib
+# fig, ax = plt.subplots()
+# ax.plot(df.index, df['2021年'])
+# ax.set_title('matplotlib graph')
+# st.pyplot(fig)
