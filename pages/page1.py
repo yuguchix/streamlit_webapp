bbc5636c483ad1c19c9b0774a821ec0c0e06e88e
yuguchix <115532108+yuguchix@users.noreply.github.com>
2023-03-23 17:14:01 +0900

    Add files via upload

--- pages/page1.py
+++ pages/page1.py
@@ -1,9 +1,147 @@
 import streamlit as st
 
-code = '''
+st.subheader('main_app.py')
+code_main = '''
 import streamlit as st
 
-st.title("さぷーアプリ")
+st.subheader('main_app.py')
+code_main = '''
+import streamlit as st
+from PIL import Image
+
+
+st.title("WEBAPP TEST")
+st.caption('Streamlibで作ったテストアプリ')
+
+st.subheader('敬慎院からの富士山')
+image = Image.open('data/aka_fuji.jpg')
+st.image(image, width=200)
+
+st.subheader('七面山崩落個所')
+video_file = open('data/shichimen.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
 '''
 
-st.code(code, language='python')
+st.code_main(code_main, language='python')
+
+st.subheader('main_app.py')
+code1 = '''
+import streamlit as st
+from PIL import Image
+
+
+st.title("WEBAPP TEST")
+st.caption('Streamlibで作ったテストアプリ')
+
+st.subheader('敬慎院からの富士山')
+image = Image.open('data/aka_fuji.jpg')
+st.image(image, width=200)
+
+st.subheader('七面山崩落個所')
+video_file = open('data/shichimen.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
+'''
+
+st.code1(code1, language='python')
+
+st.subheader('main_app.py')
+code2 = '''
+import streamlit as st
+from PIL import Image
+
+
+st.title("WEBAPP TEST")
+st.caption('Streamlibで作ったテストアプリ')
+
+st.subheader('敬慎院からの富士山')
+image = Image.open('data/aka_fuji.jpg')
+st.image(image, width=200)
+
+st.subheader('七面山崩落個所')
+video_file = open('data/shichimen.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
+'''
+
+st.code2(code2, language='python')
+
+st.subheader('main_app.py')
+code3 = '''
+import streamlit as st
+import pandas as pd
+
+
+df = pd.read_csv('data/平均気温.csv', index_col='月')
+st.subheader('平均気温')
+st.line_chart(df)
+st.subheader('2021年気温')
+st.bar_chart(df['2021年'])
+
+
+'''
+
+st.code3(code3, language='python')
+'''
+
+st.code_main(code_main, language='python')
+
+st.subheader('main_app.py')
+code1 = '''
+import streamlit as st
+from PIL import Image
+
+
+st.title("WEBAPP TEST")
+st.caption('Streamlibで作ったテストアプリ')
+
+st.subheader('敬慎院からの富士山')
+image = Image.open('data/aka_fuji.jpg')
+st.image(image, width=200)
+
+st.subheader('七面山崩落個所')
+video_file = open('data/shichimen.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
+'''
+
+st.code1(code1, language='python')
+
+st.subheader('main_app.py')
+code2 = '''
+import streamlit as st
+from PIL import Image
+
+
+st.title("WEBAPP TEST")
+st.caption('Streamlibで作ったテストアプリ')
+
+st.subheader('敬慎院からの富士山')
+image = Image.open('data/aka_fuji.jpg')
+st.image(image, width=200)
+
+st.subheader('七面山崩落個所')
+video_file = open('data/shichimen.mp4', 'rb')
+video_bytes = video_file.read()
+st.video(video_bytes)
+'''
+
+st.code2(code2, language='python')
+
+st.subheader('main_app.py')
+code3 = '''
+import streamlit as st
+import pandas as pd
+
+
+df = pd.read_csv('data/平均気温.csv', index_col='月')
+st.subheader('平均気温')
+st.line_chart(df)
+st.subheader('2021年気温')
+st.bar_chart(df['2021年'])
+
+
+'''
+
+st.code3(code3, language='python')
